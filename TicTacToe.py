ed1caa2806c173cbe1399b42def9ffa3fee6bae9
RoyalFire55555 <iamroyalfire@gmail.com>
2021-12-20 12:10:02 -0500

    Adding win detection in tictactoe

---
+++ TicTacToe.py
@@ -0,0 +1,17 @@
+
+class TicTacToe:
+    def __init__(self):
+        self.squares = {
+            "A1": "",
+            "A2": "",
+            "A3": "",
+            "A4": "",
+            "A5": "",
+            "A6": "",
+            "A7": "",
+            "A8": "",
+            "A9": ""
+        }
+
+    def checkWin(self):
+        print("in progress")
